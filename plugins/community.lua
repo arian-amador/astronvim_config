a48b59dc562a9c73a9046fe2e31d46dbb2f5c9f9
Arian Amador <arian@arianamador.com>
2023-04-05 13:52:20 -0400

    Add community language packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,10 +1,14 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  -- Color Schemes
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.tokyonight" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  -- Lang Packs
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.yaml" },
 }
